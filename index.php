c956214b5360aeb1fe8ef2ab8c0f559187acd459
ton <32086016+to-ton@users.noreply.github.com>
2020-11-14 06:53:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-hell0 world.
+<--this only intends to save the trouble of typing the google docs uri.. keep safe!--!>
+<html>
+   <body>
+      <script>
+         setTimeout(function(){
+            window.location.href = 'https://docs.google.com/spreadsheets/d/1cClZuGoPa5ZajqoV7dRS8bPNAk4iPL6KkkXPnSZ9ppE/htmlview#';
+         }, 1000);
+      </script>
+      <p>redirecting in 2 seconds.. keep safe!</p>
+   </body>
+</html>
