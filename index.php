fc44052d71334145ea7cb97b104ec8fb0f534d49
ton <32086016+to-ton@users.noreply.github.com>
2020-11-14 07:04:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!--this only intends to save the trouble of typing the google docs uri.. keep safe!-->
-<html>
-   <body>
-      <script>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>redirecting..</title>
+  </head>
+  <body>
+        <script>
          setTimeout(function(){
             window.location.href = 'https://docs.google.com/spreadsheets/d/1cClZuGoPa5ZajqoV7dRS8bPNAk4iPL6KkkXPnSZ9ppE/htmlview#';
          }, 1000);
       </script>
       <p>redirecting in 2 seconds.. keep safe!</p>
-   </body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous" type="8cefa11fdee6f876cc097a4f-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous" type="8cefa11fdee6f876cc097a4f-text/javascript"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous" type="8cefa11fdee6f876cc097a4f-text/javascript"></script>
+  <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="8cefa11fdee6f876cc097a4f-|49" defer="" type="c301f8139988cb0fffb200c7-text/javascript"></script><script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="c301f8139988cb0fffb200c7-|49" defer=""></script></body>
 </html>
