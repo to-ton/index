713b913ee8d86f73d9bde352324f2f54252582ec
ton <32086016+to-ton@users.noreply.github.com>
2020-11-14 07:00:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!--this only intends to save the trouble of typing the google docs uri.. keep safe!-->
+<html>
+   <body>
+      <script>
+         setTimeout(function(){
+            window.location.href = 'https://docs.google.com/spreadsheets/d/1cClZuGoPa5ZajqoV7dRS8bPNAk4iPL6KkkXPnSZ9ppE/htmlview#';
+         }, 1000);
+      </script>
+      <p>redirecting in 2 seconds.. keep safe!</p>
+   </body>
+</html>
