7a73bff095ed2998d8f72e158731194baaadcd22
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 07:39:51 +0800

    Update result.php

--- welcome/result.php
+++ welcome/result.php
@@ -1,6 +1,10 @@
 <?php
 if(isset($_POST["vibe"]) || isset($_POST["taste"]) || isset($_POST["setting"]) ||  isset($_POST["budget"])){
 echo "works";
+  echo $_POST["vibe"];
+  echo $_POST["taste"];
+  echo $_POST["setting"];
+  echo $_POST["budget"];
 }
 
 ?>
