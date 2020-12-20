23e995cf56156121abcf855cdcb5bdd2af169345
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 05:27:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
 				<h1><b>Choose Your Language:</b></h1>
 				<br>
 				<br>
-				<button class="button" onclick="window.location.href='/welcome'"><span>English </span></button><span> </span><button class="button"><span>Tagalog </span></button>
+				<button class="button" onclick="window.location.href='/welcome'"><span>English </span></button> <button class="button"><span>Tagalog </span></button>
 			</div>
 	</body>
 </html>
