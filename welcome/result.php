b848b7f6990d6e043fd05e01bc9b547a1cba1c8a
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 07:53:34 +0800

    Update result.php

--- welcome/result.php
+++ welcome/result.php
@@ -1,10 +1,110 @@
-<?php
-if(isset($_POST["vibe"]) || isset($_POST["taste"]) || isset($_POST["setting"]) ||  isset($_POST["budget"])){
-echo "works";
-  echo $_POST["vibe"];
-  echo $_POST["taste"];
-  echo $_POST["setting"];
-  echo $_POST["budget"];
-}
 
-?>
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">  
+    <title>Full Page Background Image | Progressive</title>
+    <style>
+
+      @import url('https://fonts.googleapis.com/css2?family=K2D&display=swap');
+
+      h1{
+        font-family: 'K2D', sans-serif;
+      }
+    
+      * { margin: 0; padding: 0; }
+          
+      html { 
+        background: url(taal_cover.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+         background-size: cover;
+       }
+          
+      #page-wrap { width: 400px; margin: 50px auto; padding: 20px; background: white; -moz-box-shadow: 0 0 20px black; -webkit-box-shadow: 0 0 20px black; box-shadow: 0 0 20px black; }
+      p { font: 15px/2 Georgia, Serif; margin: 0 0 30px 0; text-indent: 40px; }
+
+      .button {
+        display: inline-block;
+        border-radius: 4px;
+        color: #FFFFFF;
+        background-color: #339966;
+        border: none;
+        text-align: center;
+        font-size: 28px;
+        padding: 20px;
+        width: 150px;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+      }
+
+      }
+      .button {
+        display: inline-block;
+        border-radius: 4px;
+        border: none;
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 28px;
+        padding: 20px;
+        width: 200px;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+      }
+
+      .button span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+      }
+
+      .button span:after {
+        content: '\1F5F8';
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -20px;
+        transition: 0.5s;
+      }
+
+      .button:hover span {
+        padding-right: 25px;
+      }
+
+      .button:hover span:after {
+        opacity: 1;
+        right: 0;
+      }
+    </style>
+  </head>
+  <body>
+      <center></center>
+        <div id="page-wrap">
+          <h1><b>here's your bucket destination:</b></h1>
+          <br>
+          <br>
+          <?php
+            if(isset($_POST["vibe"]) == "adventure" || $_POST["taste"]) == "mountains" ||$_POST["setting"]) == "romantic" ||$_POST["budget"])){
+
+            echo "Mt. Gulugod Baboy";
+            echo "Mt. Batulao";
+            echo "Mt. Talamitam";
+            echo "Mt. Manabu";
+            echo "Mt. Daguldol"; 
+            echo "Mt. Banoi";
+            echo "Mt. Matoco";
+
+            }
+
+            ?>
+      </div>
+      </center>
+  </body>
+</html>
