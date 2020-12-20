fa2e88e91710bffb0410ee50dedb8c435c72d5ce
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 02:08:18 +0800

    Update index.php

--- welcome/1/index.php
+++ welcome/1/index.php
@@ -85,7 +85,7 @@
     </head>
     <body>
             <div id="page-wrap">
-                <form action="/q2.php" method="POST">
+                <form action="/welcome/2" method="POST">
                     <h1><b>Choose your vibe:</b></h1>
                     <input type="radio" id="adventure" name="vibe" value="adventure">
                     <label>Adventure</label><br>
