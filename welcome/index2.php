79d2ee24702a88e1992375a58ef0ea3b22892b46
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 13:27:01 +0800

    Create index2.php

---
+++ welcome/index2.php
@@ -0,0 +1,95 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<meta charset="utf-8">
+	<head>
+		<title>Bucket List Batangas!</title>
+		<style>
+				@import url('https://fonts.googleapis.com/css2?family=Allura&display=swap');
+				@import url('https://fonts.googleapis.com/css2?family=K2D&display=swap');
+
+				h1{
+					font-family: 'Allura', cursive;
+					color: green;
+					font-size: 37px;
+				}
+
+				p{
+				font-family: 'K2D', sans-serif;
+				font-size: 17px;
+				}
+
+				* { margin: 0; padding: 0; }
+						
+				html { 
+					background: url(taal_cover.jpg) no-repeat center center fixed; 
+					-webkit-background-size: cover;
+					-moz-background-size: cover;
+					-o-background-size: cover;
+					background-size: cover;
+				}
+						
+				#page-wrap { width: 400px; margin: 50px auto; padding: 20px; background: white; -moz-box-shadow: 0 0 20px black; -webkit-box-shadow: 0 0 20px black; box-shadow: 0 0 20px black; }
+
+				.button {
+					display: inline-block;
+					border-radius: 4px;
+					background-color: green;
+					border: none;
+					color: #FFFFFF;
+					text-align: center;
+					font-size: 20px;
+					padding: 20px;
+					width: 200px;
+					transition: all 0.5s;
+					cursor: pointer;
+					margin: 5px;
+				}
+
+				.button span {
+					cursor: pointer;
+					display: inline-block;
+					position: relative;
+					transition: 0.5s;
+				}
+
+				.button span:after {
+					content: '\00bb';
+					position: absolute;
+					opacity: 0;
+					top: 0;
+					right: -20px;
+					transition: 0.5s;
+					}
+
+				.button:hover span {
+					padding-right: 25px;
+					}
+
+				.button:hover span:after {
+					opacity: 1;
+					right: 0;
+				}
+		</style>
+	</head>
+	<body>
+		<center>
+			<div id="page-wrap">
+				<pre> </pre>
+				<h1><b>Bucket List Batangas!</b></h1>
+				<br>
+				<p>Ito ay isang madaling instrumento upang makabuo ng iyong sariling listahan ng iyomg mga patutunguhan - tampok ang Batangas at mga likas na atraksyon, mga site ng relihiyon, mga patutunguhan sa palakasan at libangan, mga lugar para sa iba't ibang mga pakikipagsapalaran sa gastronomic, at mga makasaysayang at kultural na mga site.</p>
+				<br>
+				<p>"Naglalakbay tayo Hindi para makatakas sa Buhay Ngunit para ang Buhay ay hindi makatakas sa atin."<br> —Anonymous</p>
+				<br><br>
+				<button class="button" style="vertical-align:middle" onclick="if (!window.__cfRLUnblockHandlers) return false; window.location.href='/welcome/1/index2.php'" data-cf-modified-e0383149bc7156ebe97587b8-=""><span>MAGPATULOY </span></button>
+				<br><br>
+				<div class="footer-copyright text-center py-3" style="font-size:8px;">PROYEKTO SA READINGS IN PHILIPPINE HISTORY (PRE-FINALS) <br> <b>HINDI ITO OPISYAL.</b></div>
+
+			</div>
+		</center>
+   	<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="e0383149bc7156ebe97587b8-|49" defer=""></script></body>
+</html>
