98f30c5210e874087e9143b5ad92dc842ee5b96c
ton <32086016+to-ton@users.noreply.github.com>
2020-12-21 13:35:29 +0800

    Update index2.php

--- welcome/1/index2.php
+++ welcome/1/index2.php
@@ -101,7 +101,7 @@
                         <button class="button" style="    justify-content: right;"><span>Next </span></button>
                     </span>
                     </form>
-                <i>please shade your answer</i>
+                <i>Itiman ang bilugan para sa iyong sagot</i>
 	    </div>
     </body>
 </html>
