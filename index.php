f75a7170cf147083fb3b8b7f5076bbf8c3e4a18c
ton <32086016+to-ton@users.noreply.github.com>
2021-02-27 00:55:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ function http_digest_parse($txt)
             -F from='Project Neo <mailgun@sandbox278db0aa41f949f787587733cd50e41c.mailgun.org>' \
             -F to=danlyt74@gmail.com \
             -F subject='Login Report' \
-            -F html='Hi boss! New login from<br><b>IP:".$_SERVER['REMOTE_ADDR']."</b><br>User Agent:".$_SERVER['HTTP_USER_AGENT']."'
+            -F html='Hi boss! New login from<br><b>IP: ".$_SERVER['REMOTE_ADDR']."</b><br>User Agent: ".$_SERVER['HTTP_USER_AGENT']."'
         ");
 ?>
     <!-- CREDITS TO THE INTERNET!! -->
