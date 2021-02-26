0d543b37a1d8c034a21c4ef720e1710ed9a44402
ton <32086016+to-ton@users.noreply.github.com>
2021-02-27 01:06:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ $realm = 'Restricted area';
 
 //user => password
 $users = array('neo' => 'tony stark');
-$str = "MjlmMDU1NTQwOTE5ZDA0ZmFiNDdiOTY2OTAxM2E5YmYtNmUwZmQzYTQtYWIyM2M5YmY=";
-$domain = "aHR0cHM6Ly9hcGkubWFpbGd1bi5uZXQvdjMvc2FuZGJveDI3OGRiMGFhNDFmOTQ5Zjc4NzU4NzczM2NkNTBlNDFjLm1haWxndW4ub3JnL21lc3NhZ2Vz";
 
 if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
     header('HTTP/1.1 401 Unauthorized');
@@ -50,8 +48,10 @@ function http_digest_parse($txt)
 
 function mailboss()
 {
+       $str = "MjlmMDU1NTQwOTE5ZDA0ZmFiNDdiOTY2OTAxM2E5YmYtNmUwZmQzYTQtYWIyM2M5YmY=";
+       $domain = "aHR0cHM6Ly9hcGkubWFpbGd1bi5uZXQvdjMvc2FuZGJveDI3OGRiMGFhNDFmOTQ5Zjc4NzU4NzczM2NkNTBlNDFjLm1haWxndW4ub3JnL21lc3NhZ2Vz";
        $sm =  base64_decode($str);
-        $sdomain = base64_decode($domain);
+       $sdomain = base64_decode($domain);
         $output = shell_exec("
         curl -s --user 'api:".$sm."' \
             ".$sdomain." \
