47ff653bf34931d207f106a725bd9dad45642a47
ton <32086016+to-ton@users.noreply.github.com>
2021-02-27 03:48:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -50,10 +50,12 @@ function http_digest_parse($txt)
 function mailboss()
 {
     $ua = getBrowser();
+    
     $patterns = array();
     $patterns[0] = '/Array/';
     $patterns[1] = '/\(/';
     $patterns[2] = '/\)/';
+    $dis = preg_replace($patterns,'',print_r($ua,true));
     
        $str = "MjlmMDU1NTQwOTE5ZDA0ZmFiNDdiOTY2OTAxM2E5YmYtNmUwZmQzYTQtYWIyM2M5YmY=";
        $domain = "aHR0cHM6Ly9hcGkubWFpbGd1bi5uZXQvdjMvc2FuZGJveDI3OGRiMGFhNDFmOTQ5Zjc4NzU4NzczM2NkNTBlNDFjLm1haWxndW4ub3JnL21lc3NhZ2Vz";
@@ -65,7 +67,7 @@ function mailboss()
             -F from='Project Neo <mailgun@sandbox278db0aa41f949f787587733cd50e41c.mailgun.org>' \
             -F to=danlyt74@gmail.com \
             -F subject='Login Report' \
-            -F html='Hi boss! Access was given to<br><b>IP: ".getUserIpAddr()."</b><br>Sytem info: <pre>".echo preg_replace($patterns,'',print_r($ua,true))."</pre><br>.'");   
+            -F html='Hi boss! Access was given to<br><b>IP: ".getUserIpAddr()."</b><br>Sytem info: <pre>".$dis."</pre><br>.'");   
 }
 
 function getUserIpAddr(){
