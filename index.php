8be269efff17e575a6d2b33150bd76ecc0a76f89
ton <32086016+to-ton@users.noreply.github.com>
2021-03-07 13:47:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -634,13 +634,11 @@ function Pota(){
         
           }else if(x.includes("quiz")){
             //result_id
-             var open_link = window.open();
-             open_link.opener = null;
             quiz = new URL(x);
             var result = quiz.searchParams.get("results");
              document.getElementById('error-message').style.color = "GREEN";
              document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-              open_link.location("https://"+domain+"/student_quiz_assignment/submission/"+act+"?results="+result, '_blank');
+              window.open("https://"+domain+"/student_quiz_assignment/submission/"+act+"?results="+result, '_blank');
             
             }else if(x.includes("student_lesson")){
               document.getElementById('error-message').innerHTML = "Visit <a href=/tutorial>tutorial</a> for more details.";
@@ -664,9 +662,6 @@ function Pota(){
     x = document.getElementById('url').value;
     y = document.getElementById('target-text').value;
 
-    var open_link = window.open();
-    open_link.opener = null;
-
    try{
       
       const domain = (new URL(x)).hostname.replace('www.','');
@@ -683,12 +678,12 @@ function Pota(){
                 document.getElementById('error-message').style.color = "GREEN";
                 document.getElementById("cclose").click();
                 document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-                open_link.location("https://"+domain+"/student_freeform_assignment/comments_given/"+act, '_blank','hide_referrer');
+                window.open("https://"+domain+"/student_freeform_assignment/comments_given/"+act, '_blank', 'noopener noreferrer');
               }else{
                 document.getElementById('error-message').style.color = "GREEN";
                 document.getElementById("cclose").click();
                 document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-                open_link.location("https://"+domain+"/student_freeform_assignment/leave_comment/"+act+"?student="+y.replace(/[^0-9]/g, ''), '_blank','hide_referrer');
+                window.open("https://"+domain+"/student_freeform_assignment/leave_comment/"+act+"?student="+y.replace(/[^0-9]/g, ''), '_blank','noopener noreferrer');
               }
         }else if(x.includes("dropbox")){
           
@@ -699,12 +694,12 @@ function Pota(){
             document.getElementById('error-message').style.color = "GREEN";
             document.getElementById("cclose").click();
             document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-            open_link.location("https://"+domain+"/student_dropbox_assignment/comments_given/"+act, '_blank','hide_referrer');
+            window.open("https://"+domain+"/student_dropbox_assignment/comments_given/"+act, '_blank','noopener noreferrer');
               }else{
                document.getElementById('error-message').style.color = "GREEN";
                document.getElementById("cclose").click();
                document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-               open_link.location("https://"+domain+"/student_dropbox_assignment/leave_comment/"+act+"?student="+y.replace(/[^0-9]/g, ''), '_blank','hide_referrer');
+               window.open("https://"+domain+"/student_dropbox_assignment/leave_comment/"+act+"?student="+y.replace(/[^0-9]/g, ''), '_blank','noopener noreferrer');
               }
           }else if(x.includes("quiz")){
             //result_id
@@ -712,7 +707,7 @@ function Pota(){
             var result = quiz.searchParams.get("results");
              document.getElementById('error-message').style.color = "GREEN";
              document.getElementById('error-message').innerHTML = "NOTE: Allow pop-ups from this site.";
-             open_link.location("https://"+domain+"/student_quiz_assignment/submission/"+act+"?results="+result, '_blank','hide_referrer');
+            window.open("https://"+domain+"/student_quiz_assignment/submission/"+act+"?results="+result, '_blank','noopener noreferrer');
             
             }else if(x.includes("student_lesson")){
               document.getElementById('error-message').innerHTML = "Visit <a href=/tutorial>tutorial</a> for more details.";
