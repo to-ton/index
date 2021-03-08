99d5f23f149de81f3be43ba75c523ad77de5c544
ton <32086016+to-ton@users.noreply.github.com>
2021-03-08 18:20:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
     header('WWW-Authenticate: Digest realm="'.$realm.
            '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
 
-    die('<code>HTTP 401. Please try again. </code>');
+    die('This is a restricted file. Authorized pips only!');
 }
 
 
@@ -342,7 +342,6 @@ function http_digest_parse($txt)
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	  <meta name="referrer" content="no-referrer" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Unauthorized Access</title>
 
@@ -424,7 +423,7 @@ function http_digest_parse($txt)
         padding-bottom: 10px;
     }
     #hist_m{
-        font-size: 10px;
+        font-size: 12px;
         color: lightgray;
         }           
     body,html{background:#1a1a1a;width:100%;height:100%;position:relative;z-index: -2;}.view{position:relative;-webkit-perspective:400;perspective:400;z-index: -2;}.plane{width:120px;height:120px;-webkit-transform-style:preserve-3d;transform-style:preserve-3d;position:absolute;z-index: -2;}.plane.main{position:absolute;top:0;left:0;right:0;bottom:0;margin:auto;-webkit-transform:rotateX(60deg) rotateZ(-30deg);transform:rotateX(60deg) rotateZ(-30deg);-webkit-animation:rotate 20s infinite linear;animation:rotate 20s infinite linear;z-index: -2;}.plane.main .circle{width:120px;height:120px;position:absolute;-webkit-transform-style:preserve-3d;transform-style:preserve-3d;border-radius:100%;box-sizing:border-box;box-shadow:0 0 60px #a10705,inset 0 0 60px #7a0000;z-index: -2;}.plane.main .circle::after,.plane.main .circle::before{content:'';display:block;position:absolute;top:0;left:0;right:0;bottom:0;margin:auto;width:5%;height:5%;border-radius:100%;background:#5d0819;box-sizing:border-box;box-shadow:0 0 60px 2px #7a0000;z-index: -2;}.plane.main .circle::before{-webkit-transform:translateZ(-90px);transform:translateZ(-90px)}.plane.main .circle::after{-webkit-transform:translateZ(90px);transform:translateZ(90px)}.plane.main .circle:nth-child(1){-webkit-transform:rotateZ(72deg) rotateX(63.435deg);transform:rotateZ(72deg) rotateX(63.435deg)}.plane.main .circle:nth-child(2){-webkit-transform:rotateZ(144deg) rotateX(63.435deg);transform:rotateZ(144deg) rotateX(63.435deg)}.plane.main .circle:nth-child(3){-webkit-transform:rotateZ(216deg) rotateX(63.435deg);transform:rotateZ(216deg) rotateX(63.435deg)}.plane.main .circle:nth-child(4){-webkit-transform:rotateZ(288deg) rotateX(63.435deg);transform:rotateZ(288deg) rotateX(63.435deg)}.plane.main .circle:nth-child(5){-webkit-transform:rotateZ(360deg) rotateX(63.435deg);transform:rotateZ(360deg) rotateX(63.435deg)}@-webkit-keyframes rotate{0%{-webkit-transform:rotateX(0) rotateY(0) rotateZ(0);transform:rotateX(0) rotateY(0) rotateZ(0)}100%{-webkit-transform:rotateX(360deg) rotateY(360deg) rotateZ(360deg);transform:rotateX(360deg) rotateY(360deg) rotateZ(360deg)}}@keyframes rotate{0%{-webkit-transform:rotateX(0) rotateY(0) rotateZ(0);transform:rotateX(0) rotateY(0) rotateZ(0)}100%{-webkit-transform:rotateX(360deg) rotateY(360deg) rotateZ(360deg);transform:rotateX(360deg) rotateY(360deg) rotateZ(360deg)}}; h2{color:whitesmoke; font-weight:bold; text-decoration:underline;}        
@@ -653,7 +652,7 @@ function Pota(){
 
    }catch(error){
     document.getElementById('error-message').style.color = "red";
-    document.getElementById('error-message').innerHTML = "Invalid URI. please include: http, https.";
+    document.getElementById('error-message').innerHTML = "Invalid URI. please include http(s)!";
     var elmnt = document.getElementById("error-message");
     elmnt.scrollIntoView();
    }
@@ -723,7 +722,7 @@ function Pota(){
 
    }catch(error){
         document.getElementById('error-message').style.color = "red";
-        document.getElementById('error-message').innerHTML = "Invalid URI. please include: http, https.";
+        document.getElementById('error-message').innerHTML = "Invalid URI. please include http(s)!";
         document.getElementById("cclose").click();
         var elmnt = document.getElementById("error-message");
         elmnt.scrollIntoView();
