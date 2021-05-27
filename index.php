743c6cdc6b9214896bf284c1295ce0b251ef23db
ton <32086016+to-ton@users.noreply.github.com>
2021-05-28 04:51:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -415,7 +415,7 @@ if (! pc_validate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) { header(
           <div class="form-group">
             <label for="message-text" class="col-form-label">https://sample.com/user/show/123</label>
             <br>
-            <i style="font-size: 10px;">You can leave it blank for the master list and click + to view answers</i>
+            <i style="font-size: 10px;">Or you can leave it blank for the master list and click + to view answers</i>
             <textarea name="targets" class="form-control" id="target-text"></textarea>
 
           </div>
